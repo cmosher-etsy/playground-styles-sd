5b2dd710f5039e63186bbd9fb6e6fbe842dcad17
Corbin Mosher <cmosher@etsy.com>
2023-01-25 22:10:45 -0600

    Package dark variables

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 let fs = import("fs");
 
 function getSCSSVariables() {
-   return fs.readFileSync("./dist/scss/_variables.scss")
+  return fs.readFileSync("./dist/scss/_variables.scss");
 }
 
-module.exports = { getSCSSVariables };
+function getSCSSDarkVariables() {
+  return fs.readFileSync("./dist/scss/_variables-dark.scss");
+}
+
+function getCSSVariables() {
+  return fs.readFileSync("./dist/scss/variables.css");
+}
+
+function getCSSDarkVariables() {
+  return fs.readFileSync("./dist/scss/variables-dark.css");
+}
+
+module.exports = {
+  getSCSSVariables,
+  getSCSSDarkVariables,
+  getCSSVariables,
+  getCSSDarkVariables,
+};
